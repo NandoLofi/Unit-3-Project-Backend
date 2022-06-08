b9a0292cab187cc03b170a2ff7b4aae0ae109c05
Fernando Galvan <n.brighter00@gmail.com>
2022-06-07 22:44:49 -0500

    changing port

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require('express')
 const app = express()
 require('dotenv').config()
 const {DATABASE_URL} = process.env
-const PORT = 3001;
 const mongoose = require('mongoose')
 const Activity = require('./models/activity')
 

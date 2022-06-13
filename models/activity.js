27223bdbfd0ffac83f9c2c134371749d270223fd
Fernando Galvan <n.brighter00@gmail.com>
2022-06-13 16:10:27 -0500

    adjusting price schema

--- models/activity.js
+++ models/activity.js
@@ -4,7 +4,7 @@ const activitySchema = new mongoose.Schema({
     type: String,
     name: String,
     desc: String,
-    price: Number,
+    price: { type: Number, multipleOf: 0.01 },
     imgFull: String,
     imgThumb: String,
     url: String,
